a7f86abbdbea359b22b023f2b38b9826b9182dcc
Vitor Hugo Salgado <2922948+vitorsalgado@users.noreply.github.com>
2026-07-06 00:49:11 +0200

    chore: remove crema

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
     projects: [
       './core/vitest.config.ts',
       './core/vitest.leak.config.ts',
-      './crema/vitest.config.ts',
       './application/vitest.config.ts',
       './http/vitest.config.ts',
       './testing/vitest.config.ts',
